6b6385d8cfbaffc4d5fb4df019f1624f9013ba7f
Tak-Pesar <106800277+Tak-Pesar@users.noreply.github.com>
2025-08-09 01:48:38 +0330

    Update Generator.php

--- src/Tl/Generator.php
+++ src/Tl/Generator.php
@@ -6,6 +6,11 @@ namespace Tak\Liveproto\Tl;
 
 abstract class Generator {
 	static public function start(mixed ...$tls) : void {
+		print('start : ');
+		var_dump($tls);
+	}
+	static public function fuck(mixed ...$tls) : void {
+		print('fuck : ');
 		var_dump($tls);
 	}
 }
