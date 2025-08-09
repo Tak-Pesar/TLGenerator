a20e356532ea7151de2068b831b18ece4ac6ada4
Tak-Pesar <106800277+Tak-Pesar@users.noreply.github.com>
2025-08-09 12:23:51 -0700

    Add files via upload

--- src/setup.php
+++ src/setup.php
@@ -11,8 +11,8 @@ use function Amp\File\write;
 function setup(string $path) : void {
 	$src = realpath($path).DIRECTORY_SEPARATOR.'src';
 	$tlpath = $src.DIRECTORY_SEPARATOR.'Tl';
-	putenv('TLPATH='.$path);
-	$_ENV['TLPATH'] = $path;
+	putenv('TLPATH='.$tlpath);
+	$_ENV['TLPATH'] = $tlpath;
 	Generator::start();
 	$errorsfile = strval($src.DIRECTORY_SEPARATOR.'Utils'.DIRECTORY_SEPARATOR.'errors.json');
 	$errorscontent = json_decode(file_get_contents('https://core.telegram.org/file/400780400470/3/OY6JMkb69K4.143326.json/3c10f72ff9ce45e8a9'),flags : JSON_THROW_ON_ERROR);
