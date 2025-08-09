9133e07c01cfdb4fe30f55ecd9b343487a949883
Tak-Pesar <106800277+Tak-Pesar@users.noreply.github.com>
2025-08-09 22:21:37 +0330

    Update Plugin.php

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Tak\Liveproto\Tl;
+namespace Tak\Liveproto;
 
 use Composer\Composer;
 
@@ -10,25 +10,22 @@ use Composer\IO\IOInterface;
 
 use Composer\Plugin\PluginInterface;
 
+use Composer\EventDispatcher\EventSubscriberInterface;
+
 use Composer\Script\Event;
 
 use Composer\Script\ScriptEvents;
 
-class Plugin implements PluginInterface {
+class Plugin implements PluginInterface , EventSubscriberInterface {
 	private Composer $composer;
 	private IOInterface $io;
+	private ? string $path = null;
 
 	public function activate(Composer $composer,IOInterface $io) : void {
 		$this->composer = $composer;
 		$this->io = $io;
 		$util = new Util($composer);
-		$path = $util->findInstallPath('tak/liveproto');
-		if(is_null($path) === false):
-			$tlpath = realpath($path);
-			var_dump($tlpath);
-			# putenv('TLPATH='.$path);
-			# $_ENV['TLPATH'] = $path;
-		endif;
+		$this->path = $util->findInstallPath('taknone/liveproto');
 	}
 	public function deactivate(Composer $composer,IOInterface $io) : void {
 	}
@@ -42,15 +39,19 @@ class Plugin implements PluginInterface {
 		);
 	}
 	public function onPostAutoloadDump(Event $event) : void {
-		$this->io->write('<info>dump...</info>');
-		
+		if(is_null($this->path) === false){
+			$this->io->write('<info>Setup...</info>');
+			setup($this->path);
+		} else {
+			$this->io->write('<error>Package path `taknone/liveproto` not found</error>');
+		}
 	}
 	public function onPostInstall(Event $event) : void {
-		$this->io->write('TLGenerator : post-install hook');
+		$this->io->write('<comment>TLGenerator : post-install hook</comment>');
 		$this->onPostAutoloadDump($event);
 	}
 	public function onPostUpdate(Event $event) : void {
-		$this->io->write('TLGenerator : post-update hook');
+		$this->io->write('<comment>TLGenerator : post-update hook</comment>');
 		$this->onPostAutoloadDump($event);
 	}
 }
